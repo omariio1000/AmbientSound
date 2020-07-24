881170acffb33efa2ca9f8940d44a1b150b20bbe
omarnassar-school <54950372+omarnassar-school@users.noreply.github.com>
2020-07-24 12:07:37 -0700

    working on audio input

--- AmbientSoundWPF/MainWindow.xaml.cs
+++ AmbientSoundWPF/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using NAudio.Wave;
 using NAudio.CoreAudioApi;
 using System.Management;
 using System.Numerics;
+using System.Windows.Threading;
 
 
 namespace AmbientSoundWPF
@@ -33,25 +34,37 @@ namespace AmbientSoundWPF
 
 
         bool running = false;
-        int rate = 44100;                       //sample rate of sound card
-        int bufferSize = (int)Math.Pow(2, 11);  //must be multiple of 2
+        DispatcherTimer timer = new DispatcherTimer();
+        
 
         public MainWindow()
         {
             InitializeComponent();
             DeviceSelect.SelectedItem = null;
             DeviceSelect.Text = "Select a Device";
-            //populateDevices();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += timer_Tick;
+            populateDevices();
+
             
-            WaveIn wi = new WaveIn();
-            wi.DeviceNumber = 0;
-            wi.WaveFormat = new NAudio.Wave.WaveFormat(rate, 1);
+
         }
 
         private void populateDevices()
         {
             List<NAudio.Wave.WaveInCapabilities> sources = new List<NAudio.Wave.WaveInCapabilities>();
+            
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+                sources.Add(NAudio.Wave.WaveIn.GetCapabilities(i));
+            }
 
+            foreach (var source in sources)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Content = source.ProductName;
+                DeviceSelect.Items.Add(item);
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -59,23 +72,42 @@ namespace AmbientSoundWPF
             if (sender.Equals(OnButton)) {
                 if (!running)
                 {
+                    //Console.Write("\nON\n\n");
                     OnButton.Content = "ON";
                     OnButton.Background = new SolidColorBrush(Color.FromArgb(255, (byte)0, (byte)255, (byte)0));
                     running = true;
+                    timer.Start();
                     
                 }
                 else
                 {
+                    //Console.Write("\nOFF\n\n");
                     OnButton.Content = "OFF";
                     OnButton.Background = new SolidColorBrush(Color.FromArgb(255, (byte)255, (byte)0, (byte)0));
                     running = false;
-
+                    timer.Stop();
                 }
-            }       
+            }   
+            
+            else if (sender.Equals(DeviceRefresh))
+            {
+                //Console.Write("\nDevices Refreshed\n\n");
+                OnButton.Content = "OFF";
+                OnButton.Background = new SolidColorBrush(Color.FromArgb(255, (byte)255, (byte)0, (byte)0));
+                running = false;
+                timer.Start();
+                //populateDevices();
+            }
         }
 
-        
-
+        void timer_Tick (object sender, EventArgs e)
+        {
+            if (DeviceSelect.SelectedItem != DeviceLabel)
+            {
+                //Console.Write("\nInput Selected\n\n");
+                //timer.Stop();
+            }
+        }
         
     }
 }
