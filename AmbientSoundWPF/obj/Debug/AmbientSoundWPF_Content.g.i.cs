986e0d5550d733574b6bcd65a69ff16e7ee620dc
omarnassar-school <54950372+omarnassar-school@users.noreply.github.com>
2020-07-29 13:26:29 -0700

    added setup

--- AmbientSoundWPF/obj/Debug/AmbientSoundWPF_Content.g.i.cs
+++ AmbientSoundWPF/obj/Debug/AmbientSoundWPF_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon1.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon96.ico")]
 
 
